758f062c987f845583d1764c3ccbd768d66c6016
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-11 22:16:05 +0530

    update Home component layout with sidebar and navigation for Doctor Admin Panel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,26 @@ const Home = () => {
       <div className="bg-[#E9E7EF] min-h-screen">
         <aside
           style={{
+            padding: "20px",
             width: "300px",
           }}
           className="bg-white h-full fixed top-0 left-0 p-2"
+        ></aside>
+        <section
+          style={{
+            marginLeft: "300px",
+            // padding: "20px",
+          }}
         >
-          <h1 className="text-3xl font-bold">Sidebar</h1>
-        </aside>
-        <section>
-          <h1 className="text-5xl font-bold"></h1>
+          <nav className="bg-white p-8 shadow-sm flex justify-between">
+            <div>
+              <h1>One </h1>
+            </div>
+            <div>
+              <h1>Two</h1>
+            </div>
+          </nav>
+          <h1 className="text-5xl font-bold px-4">Doctor Admin Panel</h1>
         </section>
       </div>
     </Layout>
