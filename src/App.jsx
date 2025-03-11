a8ab0ca9c5075c6fcbf1c468ed10c9ccbdbb9371
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-11 23:50:46 +0530

    add Dashboard, Doctor, Patient, and Device components with routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import "remixicon/fonts/remixicon.css";
 import "animate.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Doctor from "./components/Doctor";
+import Patient from "./components/Patient";
+import Device from "./components/Device";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/patient" element={<Patient />} />
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/device" element={<Device />} />
       </Routes>
     </BrowserRouter>
   );
