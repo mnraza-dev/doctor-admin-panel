9e3f86a9cd9fd55695a3e4af0480f53834e0d308
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-11 22:23:44 +0530

    enhance Home component with animations, new button, and welcome message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import "animate.css";
+import "remixicon/fonts/remixicon.css";
+
 import Layout from "./Layout";
 const Home = () => {
   return (
@@ -17,15 +20,21 @@ const Home = () => {
             // padding: "20px",
           }}
         >
-          <nav className="bg-white p-8 shadow-sm flex justify-between">
+          <nav className="bg-white p-6 shadow-sm flex justify-between">
             <div>
-              <h1>One </h1>
+              <button
+                onClick={() => console.log("click")}
+                className=" cursor-pointer hover:bg-gray-100 w-10 h-10 rounded-full "
+              >
+                <i class="ri-menu-3-line"></i>
+              </button>
             </div>
             <div>
               <h1>Two</h1>
             </div>
           </nav>
           <h1 className="text-5xl font-bold px-4">Doctor Admin Panel</h1>
+          <h1 className="text-3xl font-bold px-4">Welcome</h1>
         </section>
       </div>
     </Layout>
