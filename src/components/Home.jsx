424b842cd916cc54a709b355193fa20e2f9b014c
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-11 22:10:18 +0530

    add Layout component and update Home page structure with sidebar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import Layout from "./Layout";
 const Home = () => {
   return (
-    <div>
-        <h1 className='text-6xl'>Welcome to Home Page</h1>
-    </div>
-  )
-}
+    <Layout>
+      <div className="bg-[#E9E7EF] min-h-screen">
+        <aside
+          style={{
+            width: "300px",
+          }}
+          className="bg-white h-full fixed top-0 left-0 p-2"
+        >
+          <h1 className="text-3xl font-bold">Sidebar</h1>
+        </aside>
+        <section>
+          <h1 className="text-5xl font-bold"></h1>
+        </section>
+      </div>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
